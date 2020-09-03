5067c50a2f0b953bb959ff17ebb02abad245499a
stosikmartyna <stosik.martyna@gmail.com>
2020-09-03 13:40:23 +0200

    Updates

---
+++ precache-manifest.23ffec4c7c66d7a072c897f50c6cfe67.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ffc35cb0130251cf65a61e3fb965683",
+    "url": "/waste_sorting_app/index.html"
+  },
+  {
+    "revision": "69a4bb3eb24950aa6dbb",
+    "url": "/waste_sorting_app/static/css/main.6e530f43.chunk.css"
+  },
+  {
+    "revision": "17a132c9a1f0fc0a617b",
+    "url": "/waste_sorting_app/static/js/2.3238dcdd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/waste_sorting_app/static/js/2.3238dcdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69a4bb3eb24950aa6dbb",
+    "url": "/waste_sorting_app/static/js/main.a772333c.chunk.js"
+  },
+  {
+    "revision": "dca25bb86cc9fb248779",
+    "url": "/waste_sorting_app/static/js/runtime-main.cf4e8d82.js"
+  },
+  {
+    "revision": "e1996192b98a516646ff9a8c0c0ca90c",
+    "url": "/waste_sorting_app/static/media/Oswald-Regular.e1996192.ttf"
+  },
+  {
+    "revision": "2505bfbd9bde14a7829cc8c242a0d25c",
+    "url": "/waste_sorting_app/static/media/Ubuntu-Regular.2505bfbd.ttf"
+  }
+]);
